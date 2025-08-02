d7751d014dd80897ce0c03b1f3f373bb2bbb8858
Nederfors <nilas.alakoski@gmail.com>
2025-08-02 21:38:16 +0200

    Allow Andrik traits via Blodsband

---
+++ tests/andrik.test.js
@@ -0,0 +1,25 @@
+const assert = require('assert');
+const fs = require('fs');
+const path = require('path');
+
+const code = fs.readFileSync(path.join(__dirname, '../js/index-view.js'), 'utf8');
+const match = code.match(/if \(isMonstrousTrait\(p\)\) {([^]*?)if \(!monsterOk\)/);
+assert(match, 'monstrous trait block not found');
+const fn = new Function('p','list','lvl','isRas','storeHelper', match[1] + 'return monsterOk;');
+
+const isRas = x => (x.taggar?.typ || []).includes('Ras');
+const abilityLevel = () => 0;
+
+function canTake(list){
+  const p = { namn: 'Diminutiv', taggar: { typ: ['Monstruöst särdrag'] } };
+  return fn(p, list, 'Novis', isRas, { abilityLevel });
+}
+
+assert.strictEqual(canTake([{ namn:'Andrik', taggar:{ typ:['Ras'] } }]), true);
+assert.strictEqual(canTake([
+  { namn:'Människa', taggar:{ typ:['Ras'] } },
+  { namn:'Blodsband', race:'Andrik' }
+]), true);
+assert.strictEqual(canTake([{ namn:'Människa', taggar:{ typ:['Ras'] } }]), false);
+
+console.log('All tests passed.');
