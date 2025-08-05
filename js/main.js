0b9ebeccb4cd22e2226e7ed7da8fbffc578cbd61
Nederfors <nilas.alakoski@gmail.com>
2025-08-05 10:32:54 +0200

    Add notes page with form persistence

--- js/main.js
+++ js/main.js
@@ -179,6 +179,7 @@ function boot() {
   if (dom.traits) { renderTraits(); bindTraits(); }
   if (ROLE === 'index')     initIndex();
   if (ROLE === 'character') initCharacter();
+  if (ROLE === 'notes')     initNotes();
 }
 
 /* ===========================================================
