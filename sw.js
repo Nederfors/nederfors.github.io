e9e88e8ccb850fd6d634d62b0f9a77da41be4b5f
Nederfors <nilas.alakoski@gmail.com>
2025-08-07 10:55:56 +0200

    Add PWA support

---
+++ sw.js
@@ -0,0 +1,47 @@
+const CACHE_NAME = 'symbaroum-pwa-v1';
+const URLS_TO_CACHE = [
+  '/',
+  '/index.html',
+  '/character.html',
+  '/karaktarsblad.html',
+  '/notes.html',
+  '/css/style.css',
+  '/icons/icon_DA'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(URLS_TO_CACHE))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      if (response) {
+        return response;
+      }
+      return fetch(event.request).then(fetchResponse => {
+        return caches.open(CACHE_NAME).then(cache => {
+          cache.put(event.request, fetchResponse.clone());
+          return fetchResponse;
+        });
+      });
+    })
+  );
+});
+
+self.addEventListener('activate', event => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.map(key => {
+          if (!cacheWhitelist.includes(key)) {
+            return caches.delete(key);
+          }
+        })
+      )
+    )
+  );
+});
